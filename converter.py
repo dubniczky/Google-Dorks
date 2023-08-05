7cfb295ce319257d8bab949522f5b0d0ef1ff252
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2023-08-05 18:20:27 +0200

    Added zip packing to project

--- converter.py
+++ converter.py
@@ -21,7 +21,7 @@ dorklist = list(map(lambda x: x['dork'], dorks['dorks']))
 
 # yaml
 shutil.copyfile(DORKS_FILE, f'{directory}/{DORKS_FILE}')
-print(f'Created {DORKS_FILE}')
+print(f'Added {DORKS_FILE} to the package')
 
 # json
 name = 'dorks.json'
@@ -37,3 +37,12 @@ print(f'Created {name}')
 name = 'dorks.csv'
 write(name, '\n'.join(['Dorks', 'Descriptions'] + [f'{dork["dork"]},{dork["desc"]}' for _, dork in enumerate(dorks['dorks'])]))
 print(f'Created {name}')
+
+
+# Adding readme
+shutil.copyfile('readme.md', f'{directory}/readme.md')
+print(f'Added readme to the package')
+
+# Make archive
+shutil.make_archive('dorks', 'zip', directory)
+print(f'Created dorks.zip')
